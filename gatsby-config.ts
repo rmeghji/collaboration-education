73e5f4d6a22db4e822365fd0eb01d20ca53cf256
rmeghji <rayhan_meghji@brown.edu>
2023-11-02 13:16:13 -0400

    netlify migration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Collaboration Education`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.collaborationeducation.org`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -24,7 +24,7 @@ const config: GatsbyConfig = {
         path: `${__dirname}/src/pages`,
       },
     },
-  "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify"]
 };
 
 export default config;
